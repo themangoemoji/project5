52a59d0e78811a0f50092d5b2474762257bdde44
Michael Wright <wrigh517@gmail.com>
2014-11-14 11:50:57 -0500

    woooow

--- UnweightedGraph.cpp
+++ UnweightedGraph.cpp
@@ -113,81 +113,165 @@ bool UnweightedGraph::IsEulerian() const {
 }
 
 list<int> UnweightedGraph::FindEulerianCycle() const {
-	// Creating the lists that will keep track of the cycles
-	// Splice_into will eventually hold the finished cycle
-	std::vector<std::list<bool> > boolList;
-	list<int> subcycle;  
-	list<int> current_cycle;  
-	std::vector< std::pair<int, int> > paths;
-  // numbers to hold path start and end
-	int start = 0, end= 0;
-  // The index of the overlying adjList (node)
-  int node = 0;
-  // The current and next indecies for nodes
-  int curr, next;
-  std::pair <int, int> path;
-	path = std::make_pair (start, end);
-  
-	
-	// Load values into paths
-	for (auto iter = adjList.begin(); iter != adjList.end(); iter++)
-	{
-		for (auto listIt = iter->begin(); listIt != iter->end(); listIt++)
-		{
-			// Make paths and insert them into vector of paths
-			path.first = node;
-			path.second = *listIt;				
-      paths.push_back(path);	
-				// Reverse path and insert
-		}
-    node++;
-	}
-  
-  // Reset the node value, start next iteration.
-  // This time we are constructing cycles, and splicing into current cycle
-  node = 0;
-	// The cycle traverse
-	for (auto iter = adjList.begin(); iter != adjList.end(); iter++)
-	{
-		for (auto listIt = iter->begin(); listIt != iter->end(); listIt++)
-		{
-      start = node;
-      end = *listIt;
-      subcycle.push_back(node);
-      for (auto elem : paths)
+  // Creating the lists that will keep track of the cycles
+  // Splice_into will eventually hold the finished cycle
+  std::vector<std::list<bool> > boolList;
+  list<int> current_cycle;  
+  list<int> subcycle;  
+  // Used to terminate full cycle
+  bool paths_exhausted = false;
+
+  // These are the indecies of the start and end of a path
+  // These will also be used to set the bool values of the matrix
+  int pathStart = 0, pathEnd = 0, initial = -1, cycle_start = 0, compare_end = -1;
+
+  // We will use this to count how many nodes point to null pointers
+  // so that we can determine whether or not to terminate the cycle
+  int count_null = 0;
+
+  // Pointer to the start of the current cycle
+  // This tells us whether or not we have completed a cycle
+  // Set to greater than the size of the list initially so that we may begin the while loop
+  std::vector<std::list<int>::const_iterator> node_iters;
+
+  // Data structures for constructing bool matrix
+  std::vector<std::vector<bool> > bool_matrix;
+  std::vector<bool> bool_vec; 
+
+
+  // Iterate through length of list, insert "false" into vector of bools
+  // that signal whether or not path is visited
+  for (auto itr = 0; itr != adjList.size(); itr++)
+  {
+    bool_vec.push_back(0);
+  }
+  // Iterate through length and insert "false" lists into matrix
+  for (auto itr = 0; itr != adjList.size(); itr++)
+  {
+    bool_matrix.push_back(bool_vec);
+  }
+
+  /*
+   * SAMPLE ADJACENCY LIST
+   * 0 | 2, 3  <<-- I call the 0, 1, 2, 3, 4.... nodes (the places we can traverse from)
+   * 1 | 0, 2
+   * 2 | ....
+   * 3 | ....
+   * 4 | ....
+   */
+
+  // Iterate through adjList to get pointers to the start of each list
+  // Instantiate pointers to the begining of each of the adjacency lists (node_iters)
+  // This will let us retain pointers to the elements in each node
+  for (auto itr = adjList.begin(); itr != adjList.end(); itr++)
+  {
+    node_iters.push_back(itr->begin());
+  }
+
+  // Start the cycle process
+  // append first elem of adjList to current cycle
+  auto currItr = node_iters[0];
+  subcycle.push_back(pathStart);
+
+  while (! paths_exhausted)
+  {
+    // A single subcycle
+    while (cycle_start != compare_end)
+    {
+      if (! bool_matrix[pathStart][*node_iters[pathStart]])
       {
-        // If we have a valid, unused path
-        if (elem.first == start && elem.second == end)
+        // Add the path to the current list, 
+        // set paths to true (indicates they have been traversed)
+        pathEnd = *node_iters[pathStart];
+        if (node_iters[pathStart] != adjList[pathStart].end())
+          subcycle.push_back(pathEnd);
+        cout << "Pushing back " << pathEnd << endl;
+        cout << " pS, PE " << endl;
+        cout << pathStart << ' ' << pathEnd << endl;
+        cout << " CURR " << endl;
+        for (auto elem : subcycle)
+          cout << elem << " ";
+        cout << endl;
+        bool_matrix[pathStart][pathEnd] = 1;
+        bool_matrix[pathEnd][pathStart] = 1;
+
+        auto holdStart = *node_iters[pathStart]; 
+        //std::advance(node_iters[pathStart], 1);
+        cout << " b e f o r e  &  a f t e r " << endl;
+        cout << pathStart << ' ' << *node_iters[pathStart] << endl;
+        if (node_iters[pathStart] != adjList[pathStart].end())
+          node_iters[pathStart]++;
+        cout << pathStart << ' ' << *node_iters[pathStart] << endl;
+        pathStart = holdStart;
+        //pathStart = *node_iters[temp_Start];
+        pathEnd = *node_iters[pathStart];
+        compare_end = pathStart;
+
+      }
+      else
+      {
+        // Increment pointer to next possible node for next iteration through adjList
+        //std::advance(node_iters[pathStart], 1);
+        if(node_iters[pathStart] != adjList[pathStart].end())
         {
-          // Delete the forward and reverse paths
-          path = std::make_pair(elem.second, elem.first);
-          paths.push_back(path);
-          path = std::make_pair(elem.first, elem.second);
-          paths.push_back(path);
-          subcycle.push_back(end);
-          advance(iter, end-1);
-          for (auto elem : subcycle)
-            cout << elem << " ";
-          cout << endl;
-          break;
-        }// Of if
-      }// Of for Elements in paths
-		}// Of for Elements in lists
-    node++;
-	}// Of for Lists
+          node_iters[pathStart]++;
+        }
 
-	for (auto elem : paths)
-	{
-		cout << elem.first << ", " << elem.second << endl;
-	}
-	cout << endl;
 
-	// Starting the cycle with the first element of the first list
+      }
+    }
 
-	return current_cycle;
+
+    // Count the number of nullptr paths, if they
+    // are equal to the size of the vector of node iterators,
+    // the paths are all exhausted --> exit the Algorithm
+    for (auto i = 0; i != node_iters.size(); i++)
+    {
+      if (node_iters[i] == adjList[i].end())
+      {
+        cout << "COUNTING NULL" << endl;
+        count_null++;
+        cout << endl;
+      }
+    }
+
+    // If we should break out of the whole thing
+    if (count_null == node_iters.size())
+      paths_exhausted = true;
+    else
+      count_null = 0;
+
+
+    // Splicing sublist into current_list
+    auto currCycItr = current_cycle.begin();
+    current_cycle.splice(currCycItr, subcycle);
+
+    cout << endl << " CURRENT CYCLE" << endl;
+    for (auto elem : current_cycle)
+      cout << elem << ' ';
+    cout << endl;
+
+    cout << endl << "SUBCYCLE" << endl;
+    for (auto elem : subcycle)
+      cout << elem << ' ';
+    cout << endl;
+
+    // Continue on next loop
+    for (auto elem : subcycle)
+    {
+      if (node_iters[elem] != adjList[elem].end()) 
+      {
+        pathStart = elem;
+        pathEnd = *node_iters[elem];
+        break;
+      }
+    } 
+
+  }
+  return current_cycle;
 }
 
 bool UnweightedGraph::VerifyEulerCycle(const list<int>& cycle) const {
-	//TODO
-	return false;
+  //TODO
+  return false;
 }
